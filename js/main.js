a08a3966ea9a59b02ff73eb7b5162d6e9ab7cd2c
Vincent BILLEY <vbilley@axiome.io>
2016-01-24 15:55:19 +0100

    refactor

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
-
 function checkScroll(zero, more) {
   return function() {
-    if (window.scrollY === 0) {
-      zero();
-    } else {
-      more();
-    }
+    window.scrollY === 0
+      ? zero()
+      : more();
   }
 }
 var setHeaderClass = checkScroll(
   function() {
-    document.querySelector(".site-header").classList.remove("scrolled");
+    document
+      .querySelector(".site-header")
+      .classList
+      .remove("scrolled");
   },
   function() {
-    document.querySelector(".site-header").classList.add("scrolled");
+    document
+      .querySelector(".site-header")
+      .classList
+      .add("scrolled");
   }
 )
 window.addEventListener("scroll", setHeaderClass);
